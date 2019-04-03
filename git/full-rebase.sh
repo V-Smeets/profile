4430149e817bc2fb30404aa4bc62f944497486b8
Vincent Smeets <Vincent.VSmeets@GMail.com>
2019-04-03 20:38:52 +0200

    git/full-rebase.sh:

---
+++ git/full-rebase.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+#
+cd "$(git rev-parse --show-toplevel)"
+
+checkout()
+{
+	branch="$1"
+	echo ""
+	git checkout "$branch"
+}
+
+rebase()
+{
+	parent="$1"
+	branch="$2"
+	branchBase="base/$branch"
+	if git rev-parse --verify --quiet "$branchBase" >/dev/null
+	then
+		git rebase --onto="$parent" "$branchBase" "$branch"
+	else
+		checkout "$branch"
+		git rebase "$parent"
+	fi
+	git branch --force "$branchBase" "$parent"
+}
