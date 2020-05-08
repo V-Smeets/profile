1df1b07988ec23306e084a0485a284639bc63a81
Vincent Smeets <Vincent.VSmeets@GMail.com>
2020-05-08 18:17:20 +0200

    usr/local/bin/read-p1.sh: /dev/ttyAMA0

--- usr/local/bin/read-p1.sh
+++ usr/local/bin/read-p1.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
-/bin/stty --file /dev/ttyUSB0 115200 cs8 -icrnl
-exec /bin/cat /dev/ttyUSB0 > "/var/log/read-p1/p1-telegrams.$(date +"%F.%T").log"
+/bin/stty --file /dev/ttyAMA0 115200 cs8 -icrnl
+exec /bin/cat /dev/ttyAMA0 > "/var/log/read-p1/p1-telegrams.$(date +"%F.%T").log"
